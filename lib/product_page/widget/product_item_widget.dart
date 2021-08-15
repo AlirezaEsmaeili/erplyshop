3d8a21630be1cc69e524f2fb8a640891c6ba99ff
AlirezaEsmaeili <mralireza.esmaeili79@gmail.com>
2021-08-15 18:37:22 +0430

    add pading to row

--- lib/product_page/widget/product_item_widget.dart
+++ lib/product_page/widget/product_item_widget.dart
@@ -14,24 +14,27 @@ class ProductItemWidget extends StatelessWidget {
           color: UiColors.brown,
         ),
         height: 100,
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('AAAAAAAAAAAAA'),
-                Text(
-                  '\$' + "25.2",
-                  style: TextStyle(color: Colors.green),
-                )
-              ],
-            ),
-            Image.network(
-                'https://dkstatics-public.digikala.com/digikala-products/be2b97ed0fa5c8980c18a34cdf2f5bfbb0bcdb8e_1621519764.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90',
-            height: 80,width: 80,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('AAAAAAAAAAAAA'),
+                  Text(
+                    '\$' + "25.2",
+                    style: TextStyle(color: Colors.green),
+                  )
+                ],
+              ),
+              Image.network(
+                  'https://dkstatics-public.digikala.com/digikala-products/be2b97ed0fa5c8980c18a34cdf2f5bfbb0bcdb8e_1621519764.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90',
+              height: 80,width: 80,
 
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
